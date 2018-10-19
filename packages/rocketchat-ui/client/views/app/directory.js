eeded41e2279af3231e62d5265ef80af22a3fc65
Diego Sampaio <chinello@gmail.com>
2018-10-19 18:21:43 -0300

    Add missing t param

--- packages/rocketchat-ui/client/views/app/directory.js
+++ packages/rocketchat-ui/client/views/app/directory.js
@@ -63,7 +63,7 @@ function doDirectorySearch(config, cb) {
 				return {
 					name: result.name,
 					username: result.username,
-					createdAt: timeAgo(result.createdAt),
+					createdAt: timeAgo(result.createdAt, t),
 					peer: result.federation.peer,
 				};
 			}
